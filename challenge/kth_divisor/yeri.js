a7e6c3a204b2037a4c4177a07aaf246dc2f83b0d
Yeri Armenta <yeri.armenta@gmail.com>
2019-08-15 23:00:06 -0500

    kthDivisor

---
+++ challenge/kth_divisor/yeri.js
@@ -0,0 +1,9 @@
+function kthDivisor(n, k) {
+    let divisor = []
+    for(let i = 1; i <= n; i++ ) {
+        if(n%i === 0) {
+            divisor.push(i)
+        }
+    }
+    return divisor[k-1] ? divisor[k-1] : -1
+}
